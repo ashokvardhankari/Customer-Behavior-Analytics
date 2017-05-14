b6eb5cd5d9e03d4972a81dfd1ccc853689b87bcf
Divya Aigal <divya.aigal@gmail.com>
2017-05-13 20:13:39 -0400

    New UI layout and beautification

--- shiny_code/server.R
+++ shiny_code/server.R
@@ -1,7 +1,5 @@
 library(shiny)
-#counties <- readRDS("myShinyData.rds")
-#source("helpers.R")
-customers = read.csv("/Users/divya/OneDrive/Data Analytics/Spring 2017/DAEN 690/Code/shiny_code/Shiny_Demo.csv", encoding = "utf-8")
+customers = read.csv("/shiny_code/Shiny_Demo.csv", encoding = "utf-8")
 
 shinyServer(
   function(input, output, session) {
@@ -39,42 +37,3 @@ shinyServer(
 )
 
 
-# observeEvent(input$do, {
-#   
-#   n <- which(customers$CustomerId == input$cust)
-#   
-#   output$text_output1 = renderText({
-#     customers$Age[n]
-#   })
-#   output$text_output2 = reactive({
-#     customers$FirstPurchaseDate[n]
-#   })
-#   
-#   output$text_output3 = reactive({
-#     customers$LastPurchaseDate[n]
-#   })
-#   
-#   output$text_output4 = renderText({
-#     customers$Frequency[n]
-#   })
-#   
-#   output$text_output5 = reactive({
-#     customers$Generation[n]
-#   })
-#   output$text_output6 = reactive({
-#     customers$TotalAmount[n]
-#   })
-#   output$text_output7 = reactive({
-#     customers$Items[n]
-#   })
-#   output$text_output8 = reactive({
-#     customers$HasPromotion[n]
-#   })
-#   output$text_output9 = reactive({
-#     customers$CustomerCategory[n]
-#   })
-#   
-#         
-#output$view <- renderTable({
-  
-# })  
--- shiny_code/ui.R
+++ shiny_code/ui.R
@@ -1,5 +1,5 @@
 library(shiny)
-customers = read.csv("/Users/divya/OneDrive/Data Analytics/Spring 2017/DAEN 690/Code/shiny_code/Shiny_Demo.csv", encoding = "utf-8")
+customers = read.csv("/shiny_code/Shiny_Demo.csv", encoding = "utf-8")
 
 shinyUI(fluidPage(theme = "bootstrap.css",
                   img(src='gmuLogo.png', height = 135, width = 190),
@@ -8,7 +8,6 @@ shinyUI(fluidPage(theme = "bootstrap.css",
                   
                   titlePanel("Customer Segmentation"),
                   
-                  #sidebarLayout(
                     sidebarPanel(   
                       textInput("cust","Customer ID:",customers$CustomerId),
                       p("Some other example Customer ID's are: 3192874, 1468180, 1438925, 4433437, 3294168"),
@@ -25,25 +24,10 @@ shinyUI(fluidPage(theme = "bootstrap.css",
                       h4("Amount spent:", htmlOutput("text_output6", style = "color:blue")),
                       h4("Items purchased:", htmlOutput("text_output7", sep="," ,style = "color:blue")),
                       h4("Promotions:", htmlOutput("text_output8", style = "color:blue"))
-                      #h3("Segment Name:", htmlOutput("text_output9", style = "color:green"))
                   ),
                   
                   sidebarPanel(   
                     h2("Segment Name:", htmlOutput("text_output9", style = "color:green"))
                   )
                 
-                  #img(src='moes.jpg', height = 150, width = 200, align = "right")
 ))
-
-#; font-family: 'Calibri';font-size: 18px;
-# tags$div(
-#   
-#   tags$ul(
-#     for (i in htmlOutput("inter_text_output7"))
-#     {
-#       tags$li(tags$span(i))
-#       tags$li(tags$span("test2"))
-#       tags$li(tags$span("test3"))
-#     }
-#   )
-# ),
